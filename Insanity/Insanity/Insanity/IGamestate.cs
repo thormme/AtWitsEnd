e654829f035b3304f2c080d52427b2b6f79d56e6
Steve Luplow <luplows@umich.edu>
2013-01-18 23:40:54 -0500

    everything else

---
+++ Insanity/Insanity/Insanity/IGamestate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Insanity
+{
+    interface IGamestate
+    {
+        virtual void Update();
+        virtual void Draw();
+        virtual void Initialize();
+        virtual void LoadContent();
+        virtual void UnloadContent();
+    }
+}
