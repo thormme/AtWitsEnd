3f94eb618b00c09f02faff4ba1450fef01375de7
Steve Luplow <luplows@umich.edu>
2013-01-19 00:05:52 -0500

    Added new files

---
+++ Insanity/Insanity/Insanity/GamestateManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Insanity
+{
+    public class GamestateManager : IGamestateManager
+    {
+        Stack<IGamestate> GamestateStack;
+
+        public GamestateManager()
+        {
+            GamestateStack = new Stack<IGamestate>();
+        }
+
+        public void push(IGamestate state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void pop(IGamestate state)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public IGamestate Current
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ Insanity/Insanity/Insanity/IGamestateManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Insanity
+{
+    public interface IGamestateManager
+    {
+        void push(IGamestate state);
+        void pop(IGamestate state);
+        IGamestate Current {get; private set}
+    }
+}
