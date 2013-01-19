e7c6212783279e22475d05bc532dcfcb81810b62
mschrod <mschrod@umich.edu>
2013-01-19 09:52:28 -0500

    Player can move and detect tile collisions

--- Insanity/Insanity/Insanity/Player.cs
+++ Insanity/Insanity/Insanity/Player.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Insanity.GameStates;
 
 namespace Insanity
 {
     public class Player : Actor
     {
         // 0-fully sane 1-insane;
-        double InsanityLevel;
+        public double InsanityLevel = 0;
+        public Vector2 Velocity = new Vector2();
+
+        private float mHorizontalSpeed = 30;
+        private float mJumpSpeed = 90;
 
         public Player(Vector2 position)
         : base(position, new Vector2(120, 180), new Sprite("tiles/ground"))
@@ -19,6 +24,35 @@ namespace Insanity
         public Player(List<string> args)
             : this(new Vector2(float.Parse(args[0]), float.Parse(args[1])))
         {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            if (InsanityGame.Input.MoveLeft())
+            {
+                Velocity.X = -mHorizontalSpeed;
+            }
+            if (InsanityGame.Input.MoveRight())
+            {
+                Velocity.X = mHorizontalSpeed;
+            }
+            if (InsanityGame.Input.Jump())
+            {
+                Velocity.Y = -mJumpSpeed;
+            }
+            if (InsanityGame.Input.Pause())
+            {
+                InsanityGame.GamestateManager.Push(new PauseState());
+            }
+
+            Velocity.Y += (float)gameTime.ElapsedGameTime.TotalSeconds * 40;
+            Position += (float)gameTime.ElapsedGameTime.TotalSeconds * Velocity;
+
+            foreach (Tile tile in OwnerLevel.GetCollidingTiles(new Rectangle((int)Position.X, (int)Position.Y, (int)Size.X, (int)Size.Y)))
+            {
+                Console.WriteLine(tile.X + " " + tile.Y);
+            }
         }
     }
 }
