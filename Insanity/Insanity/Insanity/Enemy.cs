50e8c8462aeb2195e982691a0b204f4ab643aebb
Steve Luplow <luplows@umich.edu>
2013-01-19 07:50:00 -0500

    New Enemy Base class

---
+++ Insanity/Insanity/Insanity/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Insanity
+{
+    public abstract class Enemy : Actor
+    {
+        protected double visibleSanityLevel;
+        protected double harmfulSanityLevel;
+
+        public Enemy(Vector2 position, Vector2 size, Sprite sprite, double visSanLevel = 0, double harmSanLevel = 0)
+            : base(position, size, sprite)
+        {
+            visibleSanityLevel = 0;
+            harmfulSanityLevel = 0;
+        }
+    }
+}
