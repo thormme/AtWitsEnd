7a9c53bc8aae7400d8a9cb1f2958d4d12967ecf4
Steve Luplow <luplows@umich.edu>
2013-01-18 23:41:13 -0500

    one more

--- Insanity/Insanity/Insanity/IGamestate.cs
+++ Insanity/Insanity/Insanity/IGamestate.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Insanity
 {
     interface IGamestate
     {
-        virtual void Update();
-        virtual void Draw();
+        virtual void Update(GameTime gameTime);
+        virtual void Draw(GameTime gameTime);
         virtual void Initialize();
         virtual void LoadContent();
         virtual void UnloadContent();
