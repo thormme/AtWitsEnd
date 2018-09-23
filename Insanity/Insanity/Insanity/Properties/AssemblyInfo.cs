f6858d2e244acb914bc2ea2c0bf1cdcebed4df0d
Michael Schroder <michael@schroder.space>
2018-09-23 03:55:57 -0700

    WIP: Updated to work with VS2017

--- Insanity/Insanity/Insanity/Properties/AssemblyInfo.cs
+++ Insanity/Insanity/Insanity/Properties/AssemblyInfo.cs
@@ -7,22 +7,20 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Insanity")]
 [assembly: AssemblyProduct("Insanity")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("e9b36d2c-c646-40d0-8741-3d7a8f883915")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aeff170b-a6af-42c3-8900-acc660c793a2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,4 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
